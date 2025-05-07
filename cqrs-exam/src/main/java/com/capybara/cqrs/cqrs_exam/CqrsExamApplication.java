8198643e4811aa433d5838f8e8b3accb2d2f4cfb
원요엘 <rf1688@refine.co.kr>
2025-05-08 08:33:44 +0900

    init project - springboot 3.4.5 - java 21

---
+++ cqrs-exam/src/main/java/com/capybara/cqrs/cqrs_exam/CqrsExamApplication.java
@@ -0,0 +1,13 @@
+package com.capybara.cqrs.cqrs_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsExamApplication.class, args);
+	}
+
+}
---
+++ cqrs-exam/src/test/java/com/capybara/cqrs/cqrs_exam/CqrsExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capybara.cqrs.cqrs_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
